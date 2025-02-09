09700b50182b897bdf1b401afa782ecc89ea2aac
CoolCoderKid <ilyas@coolcoderkid.com>
2025-02-09 17:02:45 +0000

    first commit
    using svelte
    
    Signed-off-by: CoolCoderKid <ilyas@coolcoderkid.com>

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<nav class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+    <a href="/">home</a>
+    <a href="/about">about</a>
+</nav>
+
+<h1>home</h1>
+<p>this is the home page.</p>
